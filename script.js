c74ecc2c654cf3af627d0072d72992dad1e9a0c7
SEBAMARTINEZ0611 <martinez.sebastian17@csj-rpi.org>
2015-09-17 09:36:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function changeImage() {
   var image = document.getElementById('myImage');
   
   if (image.src.match("bulbon")){
-    image.src ="image/pic_bulboff.gif"
+    image.src = "image/pic_bulboff.gif"
   } else {
     image.src = "image/pic_bulbon.gif";
     
